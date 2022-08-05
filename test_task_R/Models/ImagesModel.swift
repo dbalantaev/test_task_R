1bf4b229fcc3bfe00f111964b0a39ee438a2ccf6
dbalantaev <1dbalantaev@gmail.com>
2022-08-05 16:56:22 +0600

    final

--- test_task_R/Models/ImagesModel.swift
+++ test_task_R/Models/ImagesModel.swift
@@ -5,30 +5,6 @@
 //  Created by Дмитрий Балантаев on 27.07.2022.
 //
 
-//import Foundation
-//
-//// MARK: - ImagesModel
-//struct ImagesModel: Codable {
-//
-//    let imagesResults: [ImagesResult]
-//
-//    enum CodingKeys: String, CodingKey {
-//        case imagesResults = "images_results"
-//    }
-//}
-//
-//// MARK: - ImagesResult
-//struct ImagesResult: Codable {
-//
-//    let thumbnail: String
-//    let original: String
-//
-//    enum CodingKeys: String, CodingKey {
-//        case thumbnail
-//        case original
-//    }
-//}
-
 import Foundation
 import UIKit
 
